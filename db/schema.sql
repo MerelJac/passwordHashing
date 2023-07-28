3318d0105150953a5ed1704cfda2723abc7977c9
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-28 10:40:33 -0700

    server is functional. skeleton code in the making

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS exercise_db;
+CREATE DATABASE exercise_db;
+
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+// const routes = require('routes');
+const sequelize = require('./connection/connections');
+
+const app = express();
+const PORT = 3001;
+
+//middleware
+app.use(express.json());
+app.use(express.static('public'));
+// to allow processing of json body
+app.use(express.urlencoded({ extended: true}));
+//activate routes
+// app.use(routes);
+
+// sequelize.sync({ force: false}).then(() => {
+//     app.listen(PORT, () => console.log(`Listening on http://localhost:3000`))
+// })
+
+app.listen(PORT, (res, req) => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
