63a55c234bcae2780d95ef149c5a45c0bb6074b4
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-31 07:23:28 -0700

    routes and models added

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+// will refer to the folder called api
+const userRoutes = require('./userRoutes');
+// any fetch call started w /api will be sent to api folder
+router.use('/users', userRoutes);
+// to use in other files
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+// npm install bcrypt for password hashing
+const bcrypt = require('bcrypt');
+// install the path to the sql js file
+const User = require('../../models/Users');
+
+// create a new User 
+router.post('/', async (req, res) => {
+    try {
+        const newUser = req.body;
+        // hash the password
+        newUser.password = await bcrypt.hash(req.body.password, 10);
+        // create newUser w the hashed password and save to DB
+        const userData = await User.create(newUser);
+        res.status(200).json(userData)
+    } catch(err) {
+        res.status(400).json(err)
+    }
+});
+
+module.exports = router;
