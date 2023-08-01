222dbcda067e7d2f21c92e163a80e5d32adaa196
Merel Jacobs <merel.burleigh@gmail.com>
2023-07-31 18:33:01 -0700

    degug alert message to disappear after reload

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,8 +1,11 @@
 const createUserForm = document.querySelector('#createUserForm');
 
+let alert = document.createElement('p');
 
 createUserForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    // clear text
+    alert.textContent = '';
 
     let username = document.querySelector('#username').value;
     let email = document.querySelector('#email').value;
@@ -22,9 +25,7 @@ createUserForm.addEventListener('submit', (event) => {
         body: JSON.stringify(formData)
     }).then((response) => response.json())
     .then((data) => {
-        console.log(data);
         if (data.err) {
-            let alert = document.createElement('p');
             alert.textContent = 'Ooops! Email already exists - please try again.'
             document.body.appendChild(alert);
             return
@@ -32,7 +33,6 @@ createUserForm.addEventListener('submit', (event) => {
         document.querySelector('#username').value = '';
         document.querySelector('#email').value = '';
         document.querySelector('#password').value = '';
-        let alert = document.createElement('p');
         alert.textContent = 'Account saved & password encrypted.'
         document.body.appendChild(alert);
     }).catch((err) => {
